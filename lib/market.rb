5e05216d9db8b8a3c7db97f1891521062e2a7b25
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-31 09:51:49 -0600

    adds market test/method for add vendor, vendor names, vendors that sell

--- lib/market.rb
+++ lib/market.rb
@@ -1,10 +1,26 @@
 class Market
-  attr_reader :name,
-              :vendors
+  attr_reader   :name
+
+  attr_accessor :vendors
 
   def initialize(name)
     @name = name
     @vendors = []
   end
 
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.has_key?(item)
+    end
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
 require './lib/market'
+require 'pry'
 
 class MarketTest < Minitest::Test
   def setup
@@ -10,14 +11,7 @@ class MarketTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Market, @m
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
+
   end
 
   def test_it_has_a_name
@@ -26,5 +20,55 @@ class MarketTest < Minitest::Test
 
   def test_it_starts_with_no_vendors_in_array
     assert_equal [], @m.vendors
+  end
+
+  def test_it_can_add_vendors
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    @m.add_vendor(vendor_1)
+    @m.add_vendor(vendor_2)
+    @m.add_vendor(vendor_3)
+
+    assert_equal [vendor_1, vendor_2, vendor_3], @m.vendors
+  end
+
+  def test_it_returns_all_vendor_names
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    @m.add_vendor(vendor_1)
+    @m.add_vendor(vendor_2)
+    @m.add_vendor(vendor_3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, @m.vendor_names
+  end
+
+  def test_it_knows_vendors_that_sell_item
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+    vendor_3.stock("Peaches", 65)
+    @m.add_vendor(vendor_1)
+    @m.add_vendor(vendor_2)
+    @m.add_vendor(vendor_3)
+
+    assert_equal [vendor_1, vendor_3], @m.vendors_that_sell("Peaches")
+    assert_equal [vendor_2], @m.vendors_that_sell("Banana Nice Cream")
   end
 end
