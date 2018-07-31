049df44d11e97ba2d9b2ac2d000ccaa7399e75b8
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-31 10:20:01 -0600

    refactor market test setup

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,6 +7,14 @@ require 'pry'
 class MarketTest < Minitest::Test
   def setup
     @m = Market.new("South Pearl Street Farmers Market")
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
+    @vendor_3.stock("Peaches", 65)
   end
 
   def test_it_exists
@@ -23,84 +31,44 @@ class MarketTest < Minitest::Test
   end
 
   def test_it_can_add_vendors
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
-    @m.add_vendor(vendor_1)
-    @m.add_vendor(vendor_2)
-    @m.add_vendor(vendor_3)
+    @m.add_vendor(@vendor_1)
+    @m.add_vendor(@vendor_2)
+    @m.add_vendor(@vendor_3)
 
-    assert_equal [vendor_1, vendor_2, vendor_3], @m.vendors
+    assert_equal [@vendor_1, @vendor_2, @vendor_3], @m.vendors
   end
 
   def test_it_returns_all_vendor_names
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
-    @m.add_vendor(vendor_1)
-    @m.add_vendor(vendor_2)
-    @m.add_vendor(vendor_3)
+    @m.add_vendor(@vendor_1)
+    @m.add_vendor(@vendor_2)
+    @m.add_vendor(@vendor_3)
 
     expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     assert_equal expected, @m.vendor_names
   end
 
   def test_it_knows_vendors_that_sell_item
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
-    @m.add_vendor(vendor_1)
-    @m.add_vendor(vendor_2)
-    @m.add_vendor(vendor_3)
+    @m.add_vendor(@vendor_1)
+    @m.add_vendor(@vendor_2)
+    @m.add_vendor(@vendor_3)
 
-    assert_equal [vendor_1, vendor_3], @m.vendors_that_sell("Peaches")
-    assert_equal [vendor_2], @m.vendors_that_sell("Banana Nice Cream")
+    assert_equal [@vendor_1, @vendor_3], @m.vendors_that_sell("Peaches")
+    assert_equal [@vendor_2], @m.vendors_that_sell("Banana Nice Cream")
   end
 
   def test_it_sorts_all_vendor_items
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
-    @m.add_vendor(vendor_1)
-    @m.add_vendor(vendor_2)
-    @m.add_vendor(vendor_3)
+    @m.add_vendor(@vendor_1)
+    @m.add_vendor(@vendor_2)
+    @m.add_vendor(@vendor_3)
 
     expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
     assert_equal expected, @m.sorted_item_list
   end
 
   def test_it_knows_its_total_inventory
-    vendor_1 = Vendor.new("Rocky Mountain Fresh")
-    vendor_1.stock("Peaches", 35)
-    vendor_1.stock("Tomatoes", 7)
-    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
-    vendor_2.stock("Banana Nice Cream", 50)
-    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
-    vendor_3 = Vendor.new("Palisade Peach Shack")
-    vendor_3.stock("Peaches", 65)
-    @m.add_vendor(vendor_1)
-    @m.add_vendor(vendor_2)
-    @m.add_vendor(vendor_3)
+    @m.add_vendor(@vendor_1)
+    @m.add_vendor(@vendor_2)
+    @m.add_vendor(@vendor_3)
 
     expected = ({"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25})
     assert_equal expected, @m.total_inventory
